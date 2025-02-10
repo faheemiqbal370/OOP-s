e4dd117055c40f9197e41c3d13c936f1f202f10c
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-02-10 20:08:39 +0530

    Lab 10-01-2025 added

---
+++ Lab 10-01-2025/Call_by_refrence.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void swap(int &x,int &y)
+{
+    int temp=0;
+    temp=x;
+    x=y;
+    y=temp;
+    cout<<x<<y;
+    cout<<"\n";
+}
+int main()
+{
+    int a,b;
+    cout<<"Enter two Numbers\n";
+    cin>>a>>b;
+    cout<<"Value of a and b before swaping\n";
+    cout<<a<<b;
+    cout<<"\n";
+
+    swap(a,b);
+    cout<<"Value after swaping\n";
+    cout<<a<<b;
+
+
+}
